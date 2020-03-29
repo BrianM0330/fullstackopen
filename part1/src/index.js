72f7ae45a170258c1960b9a7714604759d7a3e3a
BrianM0330 <brianm0330@gmail.com>
2020-03-29 16:34:37 -0500

    part1b

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+/*
+ - Javascript inside the curly braces of a function is evaluated
+ - Components are reusable, and return JSX to be rendered
+ - (props) is an object passed as a parameter, their values can be strings or results of expressions
+ - 
+*/
+const App = () => { //component with the name app
+  const name = 'pete'
+  const age = 10
+
+  return ( 
+    <div>
+      <h1> Greetings! </h1>
+      <Hello name="George" age = {26+10}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+//const App = ...
+// this is a constant variable, which is assigned the value of..
+
+/* this javascript function! 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+() => (
+  <div>
+    <p>Hello world!</p>
+  </div>
+)
+
+*/
+
+ReactDOM.render(<App />, document.getElementById('root')) 
