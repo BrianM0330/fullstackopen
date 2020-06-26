0e5ed3acc3c306070b093029365548ddac7d488b
Brian Mendoza <brian@pop-os.localdomain>
2020-06-26 16:12:19 -0500

    1.c

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,60 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => (
-    <div>
-        <h1>{props.course}</h1>
-    </div>
-)
-
-const Part = (props) => (
-    <p>
-        {props.part} {props.exercises}
-    </p>
-)
-
-const Content = (props) => (
-    <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-)
-
-
-const Total = (props) => (
-    <div>
-        <p>Total number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-)
-
-
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-
+const Hello = (props) => {
     return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
+      <div>
+        <p>
+          Hello {props.name}, you are {props.age} years old
+        </p>
+      </div>
     )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+  }
+  
+  const App = () => {
+    const name = 'Peter'
+    const age = 10
+  
+    return (
+      <div>
+        <h1>Greetings</h1>
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={name} age={age} />
+      </div>
+    )
+  }
