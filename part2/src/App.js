df814c4042b8b396662113f462d99267de171e4c
Brian Mendoza <brian@pop-os.localdomain>
2020-07-02 13:45:09 -0500

    Fixed project

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react'
+import Note from './components/Note'
 
 const App = () => {
-  const course = 'Half Stack application !!!!'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  return (
-    <div>
-
-    </div>
-  )
+  return <Course course={course} />
 }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
