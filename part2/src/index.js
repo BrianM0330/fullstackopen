85877160a8a38be11df0e1cac23957b3673e8f66
Brian Mendoza <BrianM0330@gmail.com>
2020-07-03 12:01:48 -0500

    2b
    Signed-off-by: Brian Mendoza <brian@pop-os.localdomain>

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,58 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Course from './components/Course'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Note from './components/Note'
 
-const App = () => {
-    const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('This is a new note!')
+  const [showAll, setShowAll] = useState(true)
+
+  //Sends a noteObject using state from newNote object
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length +1,
     }
-  ]
+    setNotes(notes.concat(noteObject)) //Adds note to the notes array
+    setNewNote('')
+    console.log('Button clicked!', event.target)
+  }
+  
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  //Const built with expression
+  const notesToShow = showAll
+  ? notes //if true, show notes
+  : notes.filter(note => note.important) //else, filter
 
   return (
     <div>
-      <h1>WEB DEVELOPMENT CURRICULUM</h1>
-      {courses.map(course => <Course key={course.id} course={course}/>)}
+      <h1>Notes</h1>
+      
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+
+      <ul>
+      {notes.map(note => 
+          <Note key={note.id} note={note} />
+      )}
+      </ul>
+
+      <form onSubmit={addNote}> 
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+
+      <button type="submit">
+        Click to save!
+      </button>
+      </form>
+
     </div>
   )
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
