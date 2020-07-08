52f5a55fdd07dd2d041865ba6924c32258d578ea
Brian Mendoza <BrianM0330@gmail.com>
2020-07-08 11:56:22 -0500

    form component works

--- part2/phonebook/src/components/Form.js
+++ part2/phonebook/src/components/Form.js
@@ -1,1 +1,32 @@
 import React from 'react'
+
+
+const Form = ({addName, newName, newNumber, handleName, handleNumber}) => {
+    return (
+        <form onSubmit={addName}>
+            <div>
+                Name:   
+                <input
+                    value={newName}
+                    onChange={handleName}
+                />
+            </div>
+
+            <div>
+                Number:
+                <input
+                    value={newNumber}
+                    onChange={handleNumber}
+                />
+            </div>
+
+            <div>
+                <button type="submit">
+                    Add
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default Form
