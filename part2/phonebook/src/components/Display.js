eaef86ef655be11ac3571dd11d1ae5d311a23389
Brian Mendoza <brian@pop-os.localdomain>
2020-07-08 12:43:57 -0500

    2.10 (componentified)

--- part2/phonebook/src/components/Display.js
+++ part2/phonebook/src/components/Display.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
+const noEntryStyle = {color: 'red'}
+
 const namesToDisplay = ({persons}) => {
-    return (
-        <ul>
-            <li>
-                {persons.map(person => <li>{person.name}: {person.number}</li>)}
-            </li>
-        </ul>
-    )
+    if (persons.length > 0) {
+        return (
+            <div>
+                <h2> All Entries </h2>
+                <ul>
+                    <li>
+                        {persons.map(person => <li>{person.name}: {person.number}</li>)}
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+    else {
+        const element = <h3 style={noEntryStyle}> No Entries </h3>
+        return element
+    }
 }
 
 export default namesToDisplay
