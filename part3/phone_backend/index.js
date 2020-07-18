8b7911e25a4e044c622c34fb4ca6f6875f703dc0
Brian Mendoza <brian@pop-os.localdomain>
2020-07-18 15:26:44 -0500

    installed cors

--- part3/phone_backend/index.js
+++ part3/phone_backend/index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+// app.use(express.static('build'))
 
 let phoneBook = []
 
@@ -82,6 +81,6 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () =>{
+    console.log(`Server listening in port ${PORT}`)
 })
