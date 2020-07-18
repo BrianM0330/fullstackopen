6bf771934769d230604617725d2d2853253478e9
Brian Mendoza <brian@pop-os.localdomain>
2020-07-18 13:04:54 -0500

    3.1 - 3.6 backend

--- part3/phone_backend/index.js
+++ part3/phone_backend/index.js
@@ -5,39 +5,77 @@ app.use(express.json())
 
 
 let phoneBook = [  
-    {
-        name: "Joe",
-        number: "6124124",
-        id: 1
-    },
-    {
-        name: "Bill",
-        number: "214215125",
-        id: 2
-    },
-    {
-        name: "Ada",
-        number: "6321623",
-        id: 3
-    },
-    {
-        name: "Dark",
-        number: "615151",
-        id: 4
-    },
-    {
-        name: "Mart",
-        number: "5515141",
-        id: 5
-    },
   ]
 
 app.get('/', (request, response) => {
-    response.send('<h1> THIS IS THE ROOT! </h1>')
+    response.send('<h1> DO YOUR HOMEWORK </h1>')
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(phoneBook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id) //Turn the URL {:id}
+    const thePerson = phoneBook.find(person => person.id === id)
+
+    if (thePerson)
+        response.send(thePerson)
+    else
+        response.status(404).end()
+})
+
+app.get('/info', (request, response) => {
+    let timestamp = ''
+    let date_ob = new Date()
+    timestamp = timestamp + date_ob.getTime()
+    response.send(
+    `<div> Phonebook has info for ${phoneBook.length} people </div>
+    
+    <div> Current time ${timestamp} </div>`
+    )
+})
+
+const randomID = () => {
+    const initialVal = phoneBook.length > 0
+        ? Math.floor(Math.random() * 99999)
+        : 0
+    return initialVal
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.number || !body.name) {
+        return response.status(400).json({
+            error: "Missing phone number or name field"
+        })
+    }
+
+    else if (phoneBook.find(entry => entry.name === body.name)) {
+        return response.status(400).json({
+            error: "This person already exists!"
+        })
+    }
+
+    else {
+        const newEntry = {
+            name: body.name,
+            number: body.number,
+            date: new Date(),
+            id: randomID(),
+        }
+    
+        phoneBook = phoneBook.concat(newEntry)
+        response.json(newEntry)
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phoneBook = phoneBook.filter(person => person.id !== id) //return all values BUT the one that matches id
+
+    response.status(204).end()
 })
 
 const port = 3001
