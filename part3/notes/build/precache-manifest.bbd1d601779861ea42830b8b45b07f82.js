10c3545e3370fcad6019ba8b818f3619237c6d44
Brian Mendoza <brian@pop-os.localdomain>
2020-07-18 14:15:22 -0500

    frontend deploy prep

---
+++ part3/notes/build/precache-manifest.bbd1d601779861ea42830b8b45b07f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2474ba705b362a1bbe84c4d222021e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca1ba640ee1b381f149d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc1e295ce65f56145525",
+    "url": "/static/js/2.4ea247e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ea247e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1ba640ee1b381f149d",
+    "url": "/static/js/main.05a03cb5.chunk.js"
+  },
+  {
+    "revision": "3133ac6626fc2d36aa8e",
+    "url": "/static/js/runtime-main.7361fd92.js"
+  }
+]);
