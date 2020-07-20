f8c6dbe8350ba75ed2e11964ff5e0603ed7eb4b9
Brian Mendoza <brian@pop-os.localdomain>
2020-07-20 12:09:47 -0500

    fullstack deployment

---
+++ part3/phone_backend/build/precache-manifest.bd309e5f724d18e93cf0aa6aff863774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb88710f9dd9154fc899c96bfd85ca8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0e84ae7b03a4523d5d",
+    "url": "/static/css/main.c5d68257.chunk.css"
+  },
+  {
+    "revision": "1cdd4710700305184b46",
+    "url": "/static/js/2.3ec57538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ec57538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0e84ae7b03a4523d5d",
+    "url": "/static/js/main.0e4e0078.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
