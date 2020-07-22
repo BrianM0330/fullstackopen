e99373a2e63f10ee277303a9f3bb8223147c22ea
Brian Mendoza <brian@pop-os.localdomain>
2020-07-22 12:34:44 -0500

    mongodb finalized

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express() 
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+
 
 let notes = [  
   {
@@ -34,26 +37,10 @@ const generateID = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) { //if missing content
-    return response.status(400).json({
-      error: "Content missing!"
-    })
-  }
-
-  const note = { //almost identical to NoteObject from last section
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateID(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/', (req,res) => {
@@ -65,15 +52,29 @@ app.get('/api/notes', (req,res) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) //pretty much Python's eval. Turned string from JSON into Number
-  console.log(`Fetching Note ${id}`)
-  const note = notes.find(note => note.id === id)
-  
-  //Return 404 if prompted for a nonexisting entry
-  if (note)
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  else
-    response.status(404).end()
+  })
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = new Note({
+    content:    body.content,
+    important:  body.important || false,
+    date:       new Date()
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
