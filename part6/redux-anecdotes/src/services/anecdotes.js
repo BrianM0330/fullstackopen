89baa3807b4b9ea863e0df48b43cc65fc41c284b
Brian Mendoza <BrianM0330@gmail.com>
2020-08-05 14:08:30 -0500

    6.13-6.14

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+const createAnecdote = async (content) => {
+    const object = {content, id: generateId(), votes: 0}
+    const response = await axios.post(baseUrl, object)
+    return response.data
+}
+
+export default {getAll, createAnecdote}
